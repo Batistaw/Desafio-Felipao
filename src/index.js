d12b392a0e905caec104240ed1e213fcdfa81ed2
Batistaw <wgoncalvesbatistaa@gmail.com>
2023-10-19 03:50:24 -0400

    terceira vez

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("oi")
+let nomeHeroi = "coringa"
+let nivelHeroi = 200
+let experiencia = ""
+
+
+switch(nivelHeroi){
+    case "< 1000":
+        experiencia = "Ferro"
+        break
+    case ">= 1001 >= 2000":
+
+    
+
+
+
+
+
+
+
+
+
+}
